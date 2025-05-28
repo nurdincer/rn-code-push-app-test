3328f7e2e18c71be492f095509930a67e9e93b3b
nurdincer <nsuedadincer@gmail.com>
2025-05-28 14:51:18 +0300

    workflow test

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -83,7 +83,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush Produciton Tests</Text>
+        <Text style={styles.title}>Appcircle CodePush Production Tests 4 workflow</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
 
         {updateAvailable && (
