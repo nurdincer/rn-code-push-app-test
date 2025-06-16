9dedd225cd688d0735034d21474fbd0927b72aca
nurdincer <nsuedadincer@gmail.com>
2025-06-16 13:15:47 +0300

    test 4

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -83,7 +83,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush Production Tests Worfklow Test 3</Text>
+        <Text style={styles.title}>Appcircle CodePush Production Tests Workflow Test 4</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
 
         {updateAvailable && (
