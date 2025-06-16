23a45eb6c2f44d88b9a109f6b3aecb017a6efd37
nurdincer <nsuedadincer@gmail.com>
2025-06-16 12:43:35 +0300

    worfklow test

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -83,7 +83,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush Production Tests 4 workflow</Text>
+        <Text style={styles.title}>Appcircle CodePush Production Tests Worfklow Test 1</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
 
         {updateAvailable && (
