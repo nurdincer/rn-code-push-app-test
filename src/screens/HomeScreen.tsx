87a74ef124b74f47b8b2990c873febb896b2f622
nurdincer <nsuedadincer@gmail.com>
2025-06-20 18:01:20 +0300

    Diff Prod tests staging 20Jun

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -83,7 +83,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush Staging Tests iOS Diff Workflow Test Diff</Text>
+        <Text style={styles.title}>Appcircle CodePush Staging iOS Diff Release</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
 
         {updateAvailable && (
