0ccc105598ddb248e9ed3023ca00edda5cd9a714
nurdincer <nsuedadincer@gmail.com>
2025-06-20 18:33:04 +0300

    diff Test release production 20Jun

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -85,7 +85,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush Staging Release</Text>
+        <Text style={styles.title}>Appcircle CodePush Prod Release</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
       </View>
 
