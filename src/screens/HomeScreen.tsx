55ced19ee6f6d56476f9d397da16f06bf0270047
nurdincer <nsuedadincer@gmail.com>
2025-07-23 13:05:32 +0300

    Test CodePush

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -83,7 +83,7 @@ const HomeScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.header}>
-        <Text style={styles.title}>Appcircle CodePush 30 Apr Setur Test CodePush</Text>
+        <Text style={styles.title}>Appcircle CodePush Test</Text>
         <Text style={styles.subtitle}>Current Version: {currentVersion}</Text>
 
         {updateAvailable && (
